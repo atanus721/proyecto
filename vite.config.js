e0a84dfb6c133368845fd2029ac71fa06ae9ef88
ATAGA <abrahamt@veana.com>
2023-01-04 02:10:18 -0600

    Publicar presenta 01

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+	server: {
+    	hmr: {
+		host: 'https:monitoreo.veana.net',
+    	},
+	},
+    plugins: [
+        laravel({
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
+});
+
